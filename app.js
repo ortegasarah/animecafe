1b791762db5746f3e821aa7a6ca5711a172bcf08
SerchPro <proyects_serch@hotmail.com>
2022-01-06 18:36:07 -0600

    changing the strucutre of files

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ require('./config/session.config')(app);
 const projectName = "mangakafe";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)}`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
