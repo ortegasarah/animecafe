de0496fab1639ac6c346f3d0cc993feb4c79281c
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2022-01-08 11:13:00 -0600

    partials

---
+++ routes/mangainfo.js
@@ -0,0 +1,14 @@
+// const router = require("express").Router();
+// const axios = require("axios")
+
+// app.get("/mange-:id", async (req,res) => {
+//     try {
+//       const {id} = req.params;
+//       res.render('main/mangainfo');
+//     } catch(err) {
+//       console.error(err);
+//     }
+//   });
+
+
+// module.exports = router;
