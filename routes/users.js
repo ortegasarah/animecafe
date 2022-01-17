b98a8d1f2af1ccd74c794a530e93e34e9d6311f7
SerchPro <proyects_serch@hotmail.com>
2022-01-17 16:48:39 -0600

    adding profile

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,12 @@ const { uploadFile } = require("../helpers/upload-file");
 router.get("/user-profile", async(req, res, next) => {
     try {
         if (req.session.currentUser) {
-            console.log(req.session.currentUser)
+            //console.log(req.session.currentUser)
             const { _id: idUser } = req.session.currentUser
             const folders = await axios.get(`http://localhost:3000/folder/getFolders/${idUser}`);
-            console.log(folders.data)
-            res.render('users/user-profile', { userInSession: req.session.currentUser, folders: folders.data });
+            const contentFolder = folders.data.item;
+
+            res.render('users/user-profile', { userInSession: req.session.currentUser, folders: contentFolder });
         } else {
             res.render('');
         }
