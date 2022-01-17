2185ae2e0ada76f70ebd7de2d5ce6bad23553dfb
SerchPro <proyects_serch@hotmail.com>
2022-01-17 16:46:37 -0600

    adding session config

--- config/session.config.js
+++ config/session.config.js
@@ -28,7 +28,7 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000 // 60 * 1000 ms === 1 min
+                maxAge: 6e4 * 60 // 60 * 1000 ms === 1 min
             }, // ADDED code below !!!
             store: MongoStore.create({
                 mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/mangakafe'
