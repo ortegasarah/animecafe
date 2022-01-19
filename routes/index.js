a3e3e04e22b54623dfd17b7af70812162f74682d
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2022-01-19 13:43:53 -0600

    landing page style

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const axios = require('axios');
 router.get("/", async(req, res, next) => {
     try {
         response_articles = await axios.get('https://api.jikan.moe/v3/top/anime/1/upcoming');
-        articles = response_articles.data.top.slice(0, 6);
+        articles = response_articles.data.top.slice(0, 8);
         response_top = await axios.get('https://api.jikan.moe/v3/top/anime/1');
-        top = response_top.data.top.slice(0, 6);
+        top = response_top.data.top.slice(0, 8);
         data = {
             articles,
             top
