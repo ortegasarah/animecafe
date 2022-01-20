bf31617000a29d6e4dbfdaabf451cc5f7d206760
SerchPro <proyects_serch@hotmail.com>
2022-01-19 20:43:38 -0600

    adding animecafe gmail

--- helpers/send-email.js
+++ helpers/send-email.js
@@ -5,7 +5,7 @@ async function sendEmail(Toemail, data, id) {
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
             to: Toemail,
-            from: 'sergio_eduardosandoval@outlook.es',
+            from: 'hello.animecafe@gmail.com',
             templateId: id,
             dynamic_template_data: data,
         };
